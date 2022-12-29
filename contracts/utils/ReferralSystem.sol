3916edebedb0d5bd458ad9599a005ba0a25b7d3d
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-12-30 00:43:32 +0100

    Added docs

--- contracts/utils/ReferralSystem.sol
+++ contracts/utils/ReferralSystem.sol
@@ -14,15 +14,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 //
 //  =============================================
 
+/// @title A multi-level referral system contract
+/// @notice The ReferralSystem contract can handle a multi-level scenario.
+/// Each level is characterized by a percentage. Also it is possible to create multiple ranks, each of
+/// which with a different levels structure.
+/// @dev The contract is meant to be used inside another contract (owner)
 contract ReferralSystem is Ownable {
+    /// @dev Struct representing a Level
     struct Level {
         uint256 percentage;
     }
 
+    /// @dev Struct representing a Rank
     struct Rank {
         Level[] levels;
     }
 
+    /// @dev Struct representing a RefInfo, so a model used when asking info
+    /// for a specific acount referral hierarchy
     struct RefInfo {
         address account;
         uint256 percentage;
@@ -56,20 +65,28 @@ contract ReferralSystem is Ownable {
         _;
     }
 
+    /// @notice Gets the number of available ranks
+    /// @return Number of available ranks inside the system
     function ranksLength() public view returns (uint256) {
         return ranks.length;
     }
 
+    /// @notice Adds a new empty rank
     function addRank() public onlyOwner {
         ranks.push(Rank(new Level[](0)));
     }
 
+    /// @notice Removes the last rank
     function removeRank() public onlyOwner {
         require(ranks.length > 0, "No ranks available");
 
+        // Remove rank from top
         ranks.pop();
     }
 
+    /// @notice Adds a new level for the specified rank
+    /// @param _rankIndex Index of the rank to add the level to
+    /// @param _percentage Referral percentage of the level to be created
     function addLevel(uint256 _rankIndex, uint256 _percentage)
         public
         onlyOwner
@@ -78,6 +95,10 @@ contract ReferralSystem is Ownable {
         ranks[_rankIndex].levels.push(Level(_percentage));
     }
 
+    /// @notice Edits the percentage of an existing level
+    /// @param _rankIndex Index of the rank to be edited
+    /// @param _levelIndex Index of the level to be edited
+    /// @param _percentage New level percentage
     function editLevel(
         uint256 _rankIndex,
         uint256 _levelIndex,
@@ -86,6 +107,8 @@ contract ReferralSystem is Ownable {
         ranks[_rankIndex].levels[_levelIndex].percentage = _percentage;
     }
 
+    /// @notice Removes the last level of the specified Rank
+    /// @param _rankIndex Index of the rank to be removed
     function removeLevel(uint256 _rankIndex)
         public
         onlyOwner
@@ -96,6 +119,9 @@ contract ReferralSystem is Ownable {
         ranks[_rankIndex].levels.pop();
     }
 
+    /// @notice Gets the levels belonging to the specified Rank
+    /// @param _rankIndex Index of the rank to get levels from
+    /// @return List of levels
     function getLevels(uint256 _rankIndex)
         public
         view
@@ -106,6 +132,9 @@ contract ReferralSystem is Ownable {
         return ranks[_rankIndex].levels;
     }
 
+    /// @notice Method to manually set the account rank
+    /// @param _account Account address
+    /// @param _rankIndex Index of the rank to be set
     function setAccountRank(address _account, uint256 _rankIndex)
         public
         onlyOwner
@@ -114,6 +143,10 @@ contract ReferralSystem is Ownable {
         accountRank[_account] = _rankIndex;
     }
 
+    /// @notice Creates the invitation (relationship) between inviter and invitee.
+    /// The invitee inherits the inviter's rank
+    /// @param _inviter Inviter address
+    /// @param _invitee Invitee address
     function setInvitation(address _inviter, address _invitee)
         public
         onlyOwner
@@ -133,6 +166,10 @@ contract ReferralSystem is Ownable {
         accountRank[_invitee] = accountRank[_inviter];
     }
 
+    /// @notice Adds a new referral action into the system
+    /// @dev Emits the events for each account above the one provided
+    /// @param _account Account address that is committing the action
+    /// @return The list of referral info for all the accounts above the one provided
     function addAction(address _account)
         public
         onlyOwner
@@ -149,10 +186,17 @@ contract ReferralSystem is Ownable {
         return refInfo;
     }
 
+    /// @notice Checks if the specified address has an inviter or not
+    /// @param _account Account address to be checked
+    /// @return True if account has inviter, false otherwise
     function hasInviter(address _account) public view returns (bool) {
         return inviter[_account] != address(0);
     }
 
+    /// @notice Gets the list of Referral actions
+    /// @param _account Account to read info from
+    /// @return List of referral actions, so the information about the
+    /// levels and percentages above the current account
     function getRefInfo(address _account)
         public
         view
