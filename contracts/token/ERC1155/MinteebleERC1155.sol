e81297252d57c57577cca0520424ed1d231d912f
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-01-27 00:17:47 +0100

    Added dynamic ids logic on ERC1155 contract

--- contracts/token/ERC1155/MinteebleERC1155.sol
+++ contracts/token/ERC1155/MinteebleERC1155.sol
@@ -16,14 +16,25 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 contract MinteebleERC1155 is ERC1155Supply, AccessControlEnumerable {
-    uint256[] ids;
+    uint256[] public ids;
+    bool public dynamicIdsEnabled;
 
     constructor(string memory _uri) ERC1155(_uri) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        dynamicIdsEnabled = true;
     }
 
     modifier requireAdmin(address _account) {
         require(hasRole(DEFAULT_ADMIN_ROLE, _account), "Unauthorized");
+        _;
+    }
+
+    modifier idExists(uint256 _id) {
+        bool idFound = false;
+        for (uint256 i = 0; i < ids.length; i++) {
+            if (ids[i] == _id) idFound = true;
+        }
+        require(idFound, "Id not found");
         _;
     }
 
@@ -37,12 +48,57 @@ contract MinteebleERC1155 is ERC1155Supply, AccessControlEnumerable {
         return super.supportsInterface(interfaceId);
     }
 
-    function airdrop(uint256 id, address[] memory _accounts)
+    function _addId(uint256 _id) internal {
+        for (uint256 i = 0; i < ids.length; i++) {
+            require(ids[i] != _id, "Id already exists");
+        }
+
+        ids.push(_id);
+    }
+
+    function _removeId(uint256 _id) internal {
+        bool idFound = false;
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            if (ids[i] == _id) {
+                ids[i] = ids[ids.length - 1];
+                delete ids[ids.length - 1];
+                ids.pop();
+
+                idFound = true;
+            }
+        }
+
+        require(idFound, "Id not found");
+    }
+
+    function addId(uint256 _id) public requireAdmin(msg.sender) {
+        _addId(_id);
+    }
+
+    function removeId(uint256 _id) public requireAdmin(msg.sender) {
+        _removeId(_id);
+    }
+
+    function getIds() public view returns (uint256[] memory) {
+        return ids;
+    }
+
+    function mintForAddress(
+        address _recipientAccount,
+        uint256 _id,
+        uint256 _amount
+    ) public requireAdmin(msg.sender) idExists(_id) {
+        _mint(_recipientAccount, _id, _amount, "");
+    }
+
+    function airdrop(uint256 _id, address[] memory _accounts)
         public
         requireAdmin(msg.sender)
+        idExists(_id)
     {
         for (uint256 i; i < _accounts.length; i++) {
-            _mint(_accounts[i], id, 1, "");
+            _mint(_accounts[i], _id, 1, "");
         }
     }
 }
--- test/token/ERC1155/MinteebleERC1155.js
+++ test/token/ERC1155/MinteebleERC1155.js
@@ -23,7 +23,7 @@ const expectThrowsAsync = async (method, errorMessage) => {
 describe("MinteebleERC1155", function () {
   let refInterface;
 
-  let account;
+  let accounts = [];
   let erc1155;
 
   before(async () => {
@@ -34,10 +34,133 @@ describe("MinteebleERC1155", function () {
   });
 
   it("Deployment of a basic ERC1155 token", async function () {
-    const deployedERC1155 = await erc1155.deploy(
-      "https://example.com/{id}.json"
-    );
+    await erc1155.deploy("https://example.com/{id}.json");
+  });
 
-    console.log("Deployed address:", deployedERC1155.address);
+  it("No ids available at the beginning", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    let ids = await token.getIds();
+
+    expect(ids.length).to.equal(0);
+  });
+
+  it("No ids available at the beginning", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    let ids = await token.getIds();
+
+    expect(ids.length).to.equal(0);
+  });
+
+  it("Adding new ids from admin account", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await token.addId(5);
+    await token.addId(9);
+
+    let ids = await token.getIds();
+
+    expect(ids.length).to.equal(2);
+    expect(ids[0]).to.equal(5);
+    expect(ids[1]).to.equal(9);
+  });
+
+  it("Removing new ids from admin account", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await token.addId(5);
+    await token.addId(9);
+    await token.removeId(5);
+
+    let ids = await token.getIds();
+
+    expect(ids.length).to.equal(1);
+    expect(ids[0]).to.equal(9);
+  });
+
+  it("Try removing new ids from non-admin account", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await token.addId(5);
+    await token.addId(9);
+
+    await expectThrowsAsync(() => token.connect(accounts[1]).removeId(5));
+  });
+
+  it("Try adding new ids from non-admin account", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await expectThrowsAsync(() => token.connect(accounts[1]).addId(6));
+  });
+
+  it("Try adding duplicate id", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await token.addId(9);
+    await expectThrowsAsync(() => token.addId(9));
+  });
+
+  it("Try removing non-existing id", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await expectThrowsAsync(() => token.removeId(9));
+  });
+
+  it("Admin mints tokens for address", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await token.addId(10);
+    await token.mintForAddress(accounts[1].address, 10, 4);
+
+    expect(await token.balanceOf(accounts[1].address, 10)).to.equal(4);
+  });
+
+  it("Admin try minting tokens with non existing ids", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+
+    await token.addId(9);
+
+    await expectThrowsAsync(() =>
+      token.mintForAddress(accounts[2].address, 10, 4)
+    );
+  });
+
+  it("Admin triggers airdrop", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+    let id = 0x8975;
+
+    await token.addId(id);
+    await token.airdrop(id, [
+      accounts[1].address,
+      accounts[2].address,
+      accounts[3].address,
+      accounts[4].address,
+      accounts[5].address,
+      accounts[6].address,
+    ]);
+
+    expect(await token.balanceOf(accounts[1].address, id)).to.equal(1);
+    expect(await token.balanceOf(accounts[2].address, id)).to.equal(1);
+    expect(await token.balanceOf(accounts[3].address, id)).to.equal(1);
+    expect(await token.balanceOf(accounts[4].address, id)).to.equal(1);
+    expect(await token.balanceOf(accounts[5].address, id)).to.equal(1);
+    expect(await token.balanceOf(accounts[6].address, id)).to.equal(1);
+  });
+
+  it("Admin triggers airdrop with non existing-id", async function () {
+    let token = await erc1155.deploy("https://example.com/{id}.json");
+    let id = 0x8975;
+
+    await expectThrowsAsync(() =>
+      token.airdrop(id, [
+        accounts[1].address,
+        accounts[2].address,
+        accounts[3].address,
+        accounts[4].address,
+        accounts[5].address,
+        accounts[6].address,
+      ])
+    );
   });
 });
