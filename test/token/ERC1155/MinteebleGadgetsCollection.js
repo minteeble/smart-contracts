23b5263bc606f8d5f1a06321b2ffcc2b2628d3a5
Samuele Girgenti <samuele.girgenti458@gmail.com>
2023-10-17 12:50:58 +0200

    Ability to remove variationsand gadget groups

--- test/token/ERC1155/MinteebleGadgetsCollection.js
+++ test/token/ERC1155/MinteebleGadgetsCollection.js
@@ -63,7 +63,26 @@ describe("MinteebleGadgetCollection", function () {
     expect(await token.getGadgetGroups()).to.equal(2);
   });
 
-  it("Should add variations", async () => {
+  it("Should create two empty gadget groups and remove one", async () => {
+    await token.addGadgetGroup();
+    await token.addGadgetGroup();
+
+    await token.removeGadgetGroup();
+
+    expect(await token.getGadgetGroups()).to.equal(1);
+  });
+
+  it("Should create two empty gadget groups and remove both", async () => {
+    await token.addGadgetGroup();
+    await token.addGadgetGroup();
+
+    await token.removeGadgetGroup();
+    await token.removeGadgetGroup();
+
+    expect(await token.getGadgetGroups()).to.equal(0);
+  });
+
+  it("Should add variations one by one", async () => {
     await token.addGadgetGroup();
 
     await token.addVariation(0);
@@ -71,6 +90,54 @@ describe("MinteebleGadgetCollection", function () {
     expect(await token.getGadgetGroups()).to.equal(1);
     expect(await token.getGadgetGroupVariations(0)).to.equal(1);
   })
+
+  it("Should add variations in bulk", async () => {
+    await token.addGadgetGroup();
+
+    await token.addVariations(0, 10);
+
+    expect(await token.getGadgetGroups()).to.equal(1);
+    expect(await token.getGadgetGroupVariations(0)).to.equal(10);
+  });
+
+  it("Should add variations in bulk to multiple groups", async () => {
+    await token.addGadgetGroup();
+    await token.addGadgetGroup();
+    await token.addGadgetGroup();
+
+    await token.addVariations(0, 10);
+    await token.addVariations(1, 10);
+    await token.addVariations(2, 10);
+
+    expect(await token.getGadgetGroups()).to.equal(3);
+    expect(await token.getGadgetGroupVariations(0)).to.equal(10);
+    expect(await token.getGadgetGroupVariations(1)).to.equal(10);
+    expect(await token.getGadgetGroupVariations(2)).to.equal(10);
+  });
+
+  it("Should remove variations one by one", async () => {
+    await token.addGadgetGroup();
+    await token.addVariations(0, 10);
+
+    await token.removeVariation(0);
+
+    expect(await token.getGadgetGroups()).to.equal(1);
+    expect(await token.getGadgetGroupVariations(0)).to.equal(9);
+  });
+
+  it("Should remove all variations from a group", async () => {
+    await token.addGadgetGroup();
+    await token.addVariations(0, 10);
+
+    // Remove all variations one by one
+    for (let i = 0; i < 10; i++) {
+      await token.removeVariation(0);
+    }
+
+    expect(await token.getGadgetGroups()).to.equal(1);
+    expect(await token.getGadgetGroupVariations(0)).to.equal(0);
+
+  });
 
   it("Should throw exception if trying to add variations with no gadgets groups available", async () => {
     await expectThrowsAsync(() => token.addVariation(0));
